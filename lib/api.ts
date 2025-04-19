40ef3b9d8d72c10177effd78b1bdba7996b2263c
Octavio Guardamagna <octaviog2308@gmail.com>
2025-04-19 14:50:03 -0300

    eliminando carpetas

--- lib/api.ts
+++
@@ -1,7 +0,0 @@
-export const fetchPets = async () => {
-    const response = await fetch('https://api.example.com/pets');
-    if (!response.ok) {
-        throw new Error('Error fetching pets');
-    }
-    return response.json();
-}; 
--- pages/MyPets.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Home = () => {
-    return <h1>Bienvenido a Pet Finder</h1>;
-};
-
-export default Home; 
